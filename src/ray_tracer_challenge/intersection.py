3f86d59c895d5a57ed37a0cdeed448b76c7ffa83
Johan <jdpburger@gmail.com>
2023-12-26 12:27:10 +0200

    Chapter 7: Pre-computing intersection state (#19)

--- src/ray_tracer_challenge/intersection.py
+++ src/ray_tracer_challenge/intersection.py
@@ -1,5 +1,41 @@
 from typing import Optional
 
+from src.ray_tracer_challenge.ray import Ray
+
+
+class Computations:
+    def __init__(self, t, object, point, eye_vector, normal_vector, inside):
+        self._t = t
+        self._object = object
+        self._point = point
+        self._eye_vector = eye_vector
+        self._normal_vector = normal_vector
+        self._inside = inside
+
+    @property
+    def t(self):
+        return self._t
+
+    @property
+    def object(self):
+        return self._object
+
+    @property
+    def point(self):
+        return self._point
+
+    @property
+    def eye_vector(self):
+        return self._eye_vector
+
+    @property
+    def normal_vector(self):
+        return self._normal_vector
+
+    @property
+    def inside(self):
+        return self._inside
+
 
 class Intersection():
     def __init__(self, t, object):
@@ -13,6 +49,16 @@ class Intersection():
     @property
     def object(self):
         return self._object
+
+    def prepare_computations(self, r: Ray) -> Computations:
+        point = r.position(self.t)
+        eye_vector = -r.direction
+        normal_vector = self.object.normal_at(point)
+        inside = False
+        if normal_vector.dot(eye_vector) < 0:
+            inside = True
+            normal_vector = -normal_vector
+        return Computations(self.t, self.object, point, eye_vector, normal_vector, inside)
 
 
 class Intersections:
